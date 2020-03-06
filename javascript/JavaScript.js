933387402cca35eac4987921d41b2ad440d61219
Matias Wallenius <MatiWall@live.dk>
2020-03-06 17:45:54 +0100

    dashboard

--- javascript/JavaScript.js
+++ javascript/JavaScript.js
@@ -2,3 +2,12 @@
 $(function(){
     $("#navigationPanel").load("../html/navigationPanel.html");
 });
+
+
+$(".gridster > ul").gridster({
+
+    widget_margins: [8, 8],
+
+    widget_base_dimensions: [100, 55]
+
+});
