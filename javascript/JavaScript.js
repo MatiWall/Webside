f818760b3eba1850564a08542c0f1c0c1f68d89e
Matias Wallenius <MatiWall@live.dk>
2020-03-06 17:49:37 +0100

    dashboard

--- javascript/JavaScript.js
+++ javascript/JavaScript.js
@@ -11,3 +11,35 @@ $(".gridster > ul").gridster({
     widget_base_dimensions: [100, 55]
 
 });
+
+var canvas = document.getElementById(“chart1”).getContext(“2 d”);
+
+new Chart(canvas, {
+
+    type: 'line',
+
+    data: {
+
+        labels: ["A", "B", "C", "O", "G", "W", "S"],
+
+        datasets: [{
+
+            label: ‘Company A’,
+
+            data: [12, 19, 3, 17, 6, 3, 7],
+
+            backgroundColor: "rgba(182, 213, 139, 0.5)"
+
+        }, {
+
+            label: ' Company B',
+
+            data: [2, 29, 5, 5, 2, 3, 10],
+
+            backgroundColor: "rgba(182, 133, 139, 1)"
+
+        }]
+
+    }
+
+});
